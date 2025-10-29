88568e3075537d2614ba2e530a5af9c6bd5d429e
aysla-vitoria <124089903+aysla-vitoria@users.noreply.github.com>
2025-10-29 16:58:10 -0300

    Add basic class structure in EstruturaBasica.java

---
+++ src/01-introducao/EstruturaBasica.java
@@ -0,0 +1,10 @@
+public class EstruturaBasica {
+    // Atributo
+    String mensagem = "Estrutura básica de uma classe";
+
+    // Método principal
+    public static void main(String[] args) {
+        EstruturaBasica exemplo = new EstruturaBasica();
+        System.out.println(exemplo.mensagem);
+    }
+}
