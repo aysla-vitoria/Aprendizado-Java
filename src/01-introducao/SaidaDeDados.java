32c545a8ea229c22104ea809eb46c567a03c1384
aysla-vitoria <124089903+aysla-vitoria@users.noreply.github.com>
2025-10-29 16:58:55 -0300

    Add SaidaDeDados.java for output demonstration

---
+++ src/01-introducao/SaidaDeDados.java
@@ -0,0 +1,7 @@
+public class SaidaDeDados {
+    public static void main(String[] args) {
+        System.out.print("Sem quebra de linha ");
+        System.out.println("com quebra de linha");
+        System.out.printf("Número formatado: %.2f%n", 3.14159);
+    }
+}
