0eea7990f75205deda3d0c5ac42f93f7258a3c8d
aysla-vitoria <124089903+aysla-vitoria@users.noreply.github.com>
2025-10-29 16:57:34 -0300

    Add Comentarios class demonstrating Java comments

---
+++ src/01-introducao/comentarios.java
@@ -0,0 +1,9 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentário de linha
+        /*
+         * Comentário de bloco
+         */
+        System.out.println("Exemplo de comentários em Java");
+    }
+}
