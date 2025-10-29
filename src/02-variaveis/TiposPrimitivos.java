82015605f14e175f5583d7851537a88385ce7235
aysla-vitoria <124089903+aysla-vitoria@users.noreply.github.com>
2025-10-29 17:01:09 -0300

    Add TiposPrimitivos class with basic variable types

---
+++ src/02-variaveis/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 25;
+        double altura = 1.68;
+        boolean estudante = true;
+        char genero = 'F';
+        String nome = "Aysla";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudante: " + estudante);
+        System.out.println("Gênero: " + genero);
+    }
+}
