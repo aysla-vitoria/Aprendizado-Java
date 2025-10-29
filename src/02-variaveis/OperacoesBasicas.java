a76285d36243c762c6a531353bbc0b12aa8f1909
aysla-vitoria <124089903+aysla-vitoria@users.noreply.github.com>
2025-10-29 17:01:35 -0300

    Add basic operations example in OperacoesBasicas.java

---
+++ src/02-variaveis/OperacoesBasicas.java
@@ -0,0 +1,12 @@
+public class OperacoesBasicas {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtração: " + (a - b));
+        System.out.println("Multiplicação: " + (a * b));
+        System.out.println("Divisão: " + (a / b));
+        System.out.println("Resto: " + (a % b));
+    }
+}
